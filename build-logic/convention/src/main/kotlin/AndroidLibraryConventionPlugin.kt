45d24581a9ab97c8de954d54b137a3ece6b5eda4
Angel Angeles Molina <82426196+AngelAngelesMolina@users.noreply.github.com>
2026-04-10 01:01:28 -0600

    ADD: AndroidLibraryConventionPlugin

---
+++ build-logic/convention/src/main/kotlin/AndroidLibraryConventionPlugin.kt
@@ -0,0 +1,9 @@
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class AndroidLibraryConventionPlugin  : Plugin<Project>{ //You build a plugin into other plugin
+    override fun apply(target: Project?) {
+
+    }
+
+}
